22dbd1c5ba06959a6a9fec7aaac4a73fde670a14
oneWarcraft <wjw6710@163.com>
2016-07-14 14:40:33 +0800

    布局浏览器

--- PictureBrowser/PictureBrowser/Classes/PictureBrowser/PictureBrowserController.swift
+++ PictureBrowser/PictureBrowser/Classes/PictureBrowser/PictureBrowserController.swift
@@ -13,13 +13,65 @@ class PictureBrowserController: UIViewController {
     var indexPath : NSIndexPath?
     var shops : [Shop]?
     
+    lazy var collectionView : UICollectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: UICollectionViewFlowLayout())
+    lazy var closeBtn : UIButton = UIButton()
+    lazy var saveBtn : UIButton = UIButton()
+    
     // MARK:- 系统回调函数
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.purpleColor()
+        
+        setupUI()
     }
     
-    
-    
 }
+
+extension PictureBrowserController
+{
+    func setupUI() {
+        // 1. 添加子控件
+        view.addSubview(collectionView)
+        view.addSubview(closeBtn)
+        view.addSubview(saveBtn)
+        
+        // 2. 设置子控件的位置
+        collectionView.frame = view.bounds
+        let margin : CGFloat = 20
+        let btnW : CGFloat = 90
+        let btnH : CGFloat = 32
+        let y : CGFloat = UIScreen.mainScreen().bounds.height - margin - btnH
+        closeBtn.frame = CGRect(x: margin, y: y, width: btnW, height: btnH)
+        let x : CGFloat = UIScreen.mainScreen().bounds.width - margin - btnW
+        saveBtn.frame = CGRect(x: x, y: y, width: btnW, height: btnH)
+        
+        // 3. 设置btn的相关属性
+        prepareBTN()
+        
+        // 4. 设置CollectionView的相关属性
+        prepareCollectionView()
+    }
+    
+    func prepareBTN() {
+        
+    }
+    
+    func prepareCollectionView() {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
